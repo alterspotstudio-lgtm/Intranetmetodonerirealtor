aec29e98a0d73ce368307083985268d2b631e881
Enrique Neri <alterspotstudio@gmail.com>
2026-06-08 20:47:02 -0600

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-// Método NERI · Intranet
-// Archivo conservado para estructura raíz en Vercel/GitHub.
-// La intranet actual funciona desde index.html.
-console.log('Método NERI · Intranet cargada');
